06a675c4ea0a7e0a2a95c15bf1a1861acae0e17f
Jon Thornton <thornton.jon@gmail.com>
2010-05-28 15:00:48 -0400

    Added cache stats.

--- classes/file_cache.php
+++ classes/file_cache.php
@@ -3,6 +3,8 @@
 namespace jmvc\classes;
 
 class File_Cache {
+
+	public static $stats = array('hits'=>0, 'misses'=>0, 'writes'=>0);
 	
 	private function __construct()
 	{
@@ -21,23 +23,27 @@ class File_Cache {
 	public static function get($key, $expires=0)
 	{
 		if (!IS_PRODUCTION || (defined('BUST_CACHE') && BUST_CACHE)) {
+			self::$stats['misses']++;
 			return false;
 		}
 		
 		$file = CACHE_DIR.'/'.$key.'.'.$_SERVER['HTTP_HOST'].'.cache';
 		
 		if (!file_exists($file)) {
+			self::$stats['misses']++;
 			return false;
 		}
 		
 		$fp = fopen($file, 'r');
 		if (!$fp) {
+			self::$stats['misses']++;
 			return false;
 		}
 		
 		if ($expires) {
 			$stat = fstat($fp);
 			if ($stat['mtime'] + $expires < time()) {
+				self::$stats['misses']++;
 				return false;
 			}
 		}
@@ -45,6 +51,7 @@ class File_Cache {
 		$content = fread($fp, $stat['size']);
 		fclose($fp);
 		
+		self::$stats['hits']++;
 		return $content;
 	}
 	
@@ -63,6 +70,7 @@ class File_Cache {
 		
 		fwrite($fp, $text);
 		fclose($fp);
+		self::$stats['writes']++;
 	}
 }
 ?>
