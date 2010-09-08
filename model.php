4e749d80fb41b09bd25d1aa31f3a46c54330225d
Jon Thornton <thornton.jon@gmail.com>
2010-09-08 11:39:52 -0400

    Fixed update detection of numeric fields.

--- model.php
+++ model.php
@@ -61,7 +61,11 @@ class Model {
 			$this->load();
 		}
 		
-		if ($value !== $this->_values[$key]) {
+		if (is_numeric($value) && is_numeric($this->_values[$key])) {
+			if ($value*1.0 != $this->_values[$key]*1.0) {
+				$this->_dirty_values[$key] = $value;
+			}
+		} else if (!is_numeric($value) && $value !== $this->_values[$key]) {
 			$this->_dirty_values[$key] = $value;
 		}
 	}
