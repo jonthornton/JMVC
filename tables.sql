4c3534fea166b21ad10441453b67fc4c8085ec8a
Jon Thornton <thornton.jon@gmail.com>
2011-04-25 22:08:29 -0400

    Cleaned up included SQL.

--- tables.sql
+++ tables.sql
@@ -1,11 +1,11 @@
-CREATE TABLE IF NOT EXISTS `sessions` (
-  `id` char(32) COLLATE latin1_general_ci NOT NULL,
-  `data` text COLLATE latin1_general_ci NOT NULL,
+CREATE TABLE `sessions` (
+  `id` char(32) NOT NULL,
+  `data` text NOT NULL,
   `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+) ENGINE=InnoDB;
 
-CREATE TABLE  `parkwhiz`.`postmark_mail_queue` (
+CREATE TABLE  `postmark_mail_queue` (
 `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
 `data` TEXT NOT NULL ,
 `created` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
