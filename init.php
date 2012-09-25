a5d0659b78d17fe76ffe4a3229223c05cfc0915a
Jon Thornton <thornton.jon@gmail.com>
2012-09-25 12:05:14 -0400

    Request URI cleaning

--- init.php
+++ init.php
@@ -50,7 +50,7 @@ class JMVC {
 		}
 
 		// define some helper constants
-		$uri = strip_tags(urldecode($_SERVER['REQUEST_URI']));	// defend against XSS
+		$uri = htmlspecialchars(urldecode($_SERVER['REQUEST_URI']));	// defend against XSS
 		if ($qPos = strpos($uri, '?')) {
 			define('CURRENT_URL', substr($uri, 0, $qPos));
 			define('QUERY_STRING', substr($uri, $qPos));
